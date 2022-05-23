935e4a5c4633c14d9c0acbdfdb4b1d4ac9596579
NicolasBNZ <nicolas.benzadon@gmail.com>
2022-05-23 13:20:31 +0200

    :art: test on font display

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,13 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <div className='flex flex-col items-center'>
+    <h1 className="text-3xl font-bold flex item-center group-hover:text-blue-200">
       Hello world!
     </h1>
+    <p className='customtext'>
+      Coucou
+    </p>
+ </div>
   )
 }
