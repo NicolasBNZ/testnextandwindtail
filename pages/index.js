fe0864db877b79f4627abdf6dd2900afc59f9f9e
NicolasBNZ <nicolas.benzadon@gmail.com>
2022-05-25 12:56:00 +0200

    :sparkles: display posts titles in 'normal' react

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,33 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+
+const [posts, setPosts] = useState([]);
+
+useEffect (() => {
+  const data = fetch('https://jsonplaceholder.typicode.com/posts?_limit=4')
+  .then(response => response.json())
+  .then(setPosts)
+  }, []);
+
   return (
-    <div className='flex flex-col items-center'>
-    <h1 className="text-3xl font-bold flex item-center group-hover:text-blue-200">
-      Hello world!
-    </h1>
-    <p className='customtext'>
-      Coucou
-    </p>
- </div>
+    <>
+ <Head>
+<title>
+  Page test 
+</title>
+</Head>
+<ul>
+{posts.map(post => <li>
+<h3>{post.title}</h3>
+</li>)}
+  
+</ul>
+    </>
+   
   )
 }
+
